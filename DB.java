9c8cfe00ce789eef95c052ce727d3caac5aeaa61
faddiDev <faddi.dev@gmail.com>
2020-10-25 09:42:21 +0700

    java smart game cli

--- DB.java
+++ DB.java
@@ -1,9 +1,5 @@
 /*
 Author    : Faddi Susanto
-Important : You can edit or share this application but do 
-            not do commercial as the author are not take credit from it
-Penting   : Silahkan meng-edit atau membagikan aplikasi ini namun dilarang
-            keras melakukan penjualan karena pencipta aplikasi tidak meminta uang
 */
 
 import java.io.*;
